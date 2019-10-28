753900e4893737698a95d93086574d350e5a3604
Cleber Tomazoni <clebertomazoni@hotmail.com>
2019-10-27 23:33:37 -0300

    Parte inicial da tela

--- src/login/Login.java
+++ src/login/Login.java
@@ -2,8 +2,21 @@ package login;
 
 public class Login {
 
+//    Adriner
     private static String username = "1505";
     private static String password = "yjteo";
+//    
+//    Cleber
+//    private static String username = "8235";
+//    private static String password = "cwmya";
+//    
+//    Pedro
+//    private static String username = "7778";
+//    private static String password = "xjrju";
+//    
+//    Gabriel
+//    private static String username = "";
+//    private static String password = "";
 
     public static String getUserId() {
         return username;
